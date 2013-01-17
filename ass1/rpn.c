d9ef3928eaf605bbd3adc21db4a4a18e8f095d31
Emil Taylor Bye <phptph@gmail.com>
2013-01-17 17:44:05 +0100

    Implemented push

--- ass1/rpn.c
+++ ass1/rpn.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "rpn.h"
@@ -19,7 +20,30 @@ RpnCalc newRpnCalc(){
 }
 
 void push(RpnCalc* rpnCalc, double n){
-    // TODO push n on stack, expand stack if neccesary
+    /*
+     * We want to add an element to the (rpnCalc.top + 1)-nth position and
+     * increase rpnCalc.top by one. To keep things easy we first increase top
+     * and then check whether we need to realloc before adding n to the stack.
+     */
+
+    rpnCalc->top++;
+
+    if (rpnCalc->top >= rpnCalc->size) {
+        rpnCalc->size = rpnCalc->top + 1;
+        rpnCalc->stack = realloc(rpnCalc->stack, sizeof(*(rpnCalc->stack)) * rpnCalc->size);
+
+        if (rpnCalc->stack == NULL) {
+            /*
+             * We failed to increase the size of the stack as needed, and abort
+             * the process. Freeing the stack is not required, as the process
+             * aborts anyways.
+             */
+            fputs("Failed to increase stack size, aborting.\n", stderr);
+            abort();
+        }
+    }
+
+    rpnCalc->stack[rpnCalc->top] = n;
 }
 
 void performOp(RpnCalc* rpnCalc, char op){
