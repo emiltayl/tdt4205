108748255457bbd96e5de316677e73fb45d8c26f
Emil Taylor Bye <phptph@gmail.com>
2013-01-17 16:55:32 +0100

    Added files provided for assignment 1

---
+++ ass1/rpn.c
@@ -0,0 +1,17 @@
+#include "rpn.h"
+
+RpnCalc newRpnCalc(){
+    // TODO initialize new RpnCalc
+}
+
+void push(RpnCalc* rpnCalc, double n){
+    // TODO push n on stack, expand stack if neccesary
+}
+
+void performOp(RpnCalc* rpnCalc, char op){
+    // TODO perform operation
+}
+
+double peek(RpnCalc* rpnCalc){
+    // TODO return top element of stack
+}
