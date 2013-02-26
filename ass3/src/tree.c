e44487fa3683f44cf83365446a0ace1de406612c
Emil Taylor Bye <phptph@gmail.com>
2013-02-26 17:31:18 +0100

    Working version with some refactoring.

--- ass3/src/tree.c
+++ ass3/src/tree.c
@@ -5,7 +5,7 @@
 #include "tree.h"
 
 /*
- * Macro for fetching integer-values for node_t's that contain integers.
+ * Macro for dereferencing data in a node_t that contain an integer.
  */
 #define INTVAL(n) *((int*)n->data)
 
@@ -36,10 +36,10 @@ node_print ( FILE *output, node_t *root, uint32_t nesting )
 
 /*
  * A function to change a node with it's first child. To do this we store a copy
- * of the parent node, move the child node into the parent's position.
+ * of the parent node and move the child node into the parent's position.
  * Then we free the space previously used by the child, but only after changing
- * the data- and children-fields of the child with those of the parents so we
- * don't lose any data.
+ * the data- and children-fields at that position with those of the parents so
+ * we don't lose any data.
  */
 static void collapse_node(node_t* node) {
     node_t parent = *node;
@@ -51,10 +51,27 @@ static void collapse_node(node_t* node) {
     node_finalize(parent.children[0]);
 }
 
-node_t* simplify_tree ( node_t* node ){
-    /* Temporary pointer used for constant expression calulations */
-    int* exp_tmp;
+/*
+ * A function to make the various list-types flat. It works by increasing the
+ * size of the left child's array of children by one, and then adding the right
+ * child to the end of that array. Then we collapse the current node, bringing
+ * the left child up as the current node.
+ */
+static void collapse_list(node_t* node) {
+    node->children[0]->n_children++;
+    node->children[0]->children = realloc(node->children[0]->children, sizeof(*node->children) * node->children[0]->n_children);
 
+    if (node->children[0]->children == NULL) {
+        fputs("Failed to reallocate space to make flat lists\n", stderr);
+        abort();
+    }
+
+    node->children[0]->children[node->children[0]->n_children - 1] = node->children[1];
+
+    collapse_node(node);
+}
+
+node_t* simplify_tree ( node_t* node ){
     if ( node != NULL ){
         // Recursively simplify the children of the current node
         for ( uint32_t i=0; i<node->n_children; i++ ){
@@ -69,40 +86,29 @@ node_t* simplify_tree ( node_t* node ){
             case FUNCTION_LIST: case STATEMENT_LIST: case PRINT_LIST:
             case EXPRESSION_LIST: case VARIABLE_LIST:
                 if (node->children[0]->type.index == node->type.index) {
-                    node->children[0]->n_children++;
-                    node->children[0]->children = realloc(node->children[0]->children, sizeof(*node->children) * node->children[0]->n_children);
-
-                    if (node->children[0]->children == NULL) {
-                        fputs("Failed to reallocate space to make flat lists\n", stderr);
-                        abort();
-                    }
-
-                    node->children[0]->children[node->children[0]->n_children - 1] = node->children[1];
-
-                    collapse_node(node);
+                    collapse_list(node);
                 }
                 break;
 
             // Declaration lists should also be flattened, but their stucture is sligthly
             // different, so they need their own case
             case DECLARATION_LIST:
-                 if (node->children[0] == NULL) {
-                    node->children[0] = node->children[1];
-                    node->children[1] = NULL;
-                    collapse_node(node);
-                } else if (node->children[0]->type.index == node->type.index) {
-                    node->children[0]->n_children++;
-                    node->children[0]->children = realloc(node->children[0]->children, sizeof(*node->children) * node->children[0]->n_children);
+                if (node->children[0] == NULL) {
+                    for (int i = 1; i < node->n_children; i++) {
+                        node->children[i - 1] = node->children[i];
+                    }
 
-                    if (node->children[0]->children == NULL) {
+                    node->n_children--;
+                    node->children = realloc(node->children, sizeof(*node->children) * node->n_children);
+
+                    if (node->children == NULL) {
                         fputs("Failed to reallocate space to make flat lists\n", stderr);
                         abort();
                     }
-
-                    node->children[0]->children[node->children[0]->n_children - 1] = node->children[1];
-
-                    collapse_node(node);
-                }               break;
+                } else if (node->children[0]->type.index == node->type.index) {
+                    collapse_list(node);
+                }
+                break;
 
             // These have only one child, so they are not needed
             case STATEMENT: case PARAMETER_LIST: case ARGUMENT_LIST:
@@ -114,28 +120,46 @@ node_t* simplify_tree ( node_t* node ){
             case EXPRESSION:
                 if (node->n_children == 1 && node->data == NULL) {
                     collapse_node(node);
+                } else if (node->n_children == 1 && node->children[0]->type.index == INTEGER && strcmp(node->data, "-") == 0) {
+                    /*
+                     * Unary minus, multiply the value stored in the only child
+                     * node with -1 and collapse this node.
+                     */
+                    INTVAL(node->children[0]) *= -1;
+                    collapse_node(node);
                 } else if (node->n_children == 2 && node->children[0]->type.index == INTEGER && node->children[1]->type.index == INTEGER) {
-                    exp_tmp = malloc(sizeof(*exp_tmp));
+                    /*
+                     * We perform all operations on the first child's data.
+                     */
 
                     /* Ugly section, just calculations. */
-                    if (strcmp(node->data, "+") == 0) { *exp_tmp = INTVAL(node->children[0]) + INTVAL(node->children[1]); }
-                    else if (strcmp(node->data, "-") == 0) { *exp_tmp = INTVAL(node->children[0]) - INTVAL(node->children[1]); }
-                    else if (strcmp(node->data, "*") == 0) { *exp_tmp = INTVAL(node->children[0]) * INTVAL(node->children[1]); }
-                    else if (strcmp(node->data, "/") == 0) { *exp_tmp = INTVAL(node->children[0]) / INTVAL(node->children[1]); }
-                    else if (strcmp(node->data, ">") == 0) { *exp_tmp = INTVAL(node->children[0]) > INTVAL(node->children[1]); }
-                    else if (strcmp(node->data, "<") == 0) { *exp_tmp = INTVAL(node->children[0]) < INTVAL(node->children[1]); }
-                    else if (strcmp(node->data, "<=") == 0) { *exp_tmp = INTVAL(node->children[0]) <= INTVAL(node->children[1]); }
-                    else if (strcmp(node->data, ">=") == 0) { *exp_tmp = INTVAL(node->children[0]) >= INTVAL(node->children[1]); }
-                    else if (strcmp(node->data, "==") == 0) { *exp_tmp = INTVAL(node->children[0]) == INTVAL(node->children[1]); }
-                    else if (strcmp(node->data, "!=") == 0) { *exp_tmp = INTVAL(node->children[0]) != INTVAL(node->children[1]); }
+                    if (strcmp(node->data, "+") == 0) { INTVAL(node->children[0]) += INTVAL(node->children[1]); }
+                    else if (strcmp(node->data, "-") == 0) { INTVAL(node->children[0]) -= INTVAL(node->children[1]); }
+                    else if (strcmp(node->data, "*") == 0) { INTVAL(node->children[0]) *= INTVAL(node->children[1]); }
+                    else if (strcmp(node->data, "/") == 0) { INTVAL(node->children[0]) /= INTVAL(node->children[1]); }
+                    else if (strcmp(node->data, ">") == 0) { INTVAL(node->children[0]) = INTVAL(node->children[0]) > INTVAL(node->children[1]); }
+                    else if (strcmp(node->data, "<") == 0) { INTVAL(node->children[0]) = INTVAL(node->children[0]) < INTVAL(node->children[1]); }
+                    else if (strcmp(node->data, "<=") == 0) { INTVAL(node->children[0]) = INTVAL(node->children[0]) <= INTVAL(node->children[1]); }
+                    else if (strcmp(node->data, ">=") == 0) { INTVAL(node->children[0]) = INTVAL(node->children[0]) >= INTVAL(node->children[1]); }
+                    else if (strcmp(node->data, "==") == 0) { INTVAL(node->children[0]) = INTVAL(node->children[0]) == INTVAL(node->children[1]); }
+                    else if (strcmp(node->data, "!=") == 0) { INTVAL(node->children[0]) = INTVAL(node->children[0]) != INTVAL(node->children[1]); }
+
+                    /*
+                     * Free the current node data, copy the result to the
+                     * current node and remove the pointer to the result from
+                     * the first child, before free-ing up the space used by
+                     * the children.
+                     */
+                    free(node->data);
+                    node->data = node->children[0]->data;
+                    node->children[0]->data = NULL;
 
                     node_finalize(node->children[0]);
                     node_finalize(node->children[1]);
-                    free(node->data);
                     free(node->children);
 
                     /* Write new data over current node */
-                    node_init(node, integer_n, exp_tmp, 0);
+                    node_init(node, integer_n, node->data, 0);
                 }
                 break;
         }
